07594a9bb5d0db0e11d0ff6407c38f3180dad1a5
subroh0508 <subroh.0508@gmail.com>
2018-07-29 22:44:03 +0900

    [create] create sample3 module

--- app/src/main/kotlin/net/subroh0508/drawabletest/FragmentAdapter.kt
+++ app/src/main/kotlin/net/subroh0508/drawabletest/FragmentAdapter.kt
@@ -6,12 +6,14 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import net.subroh0508.sample1.ExampleFragment1
 import net.subroh0508.sample2.ExampleFragment2
+import net.subroh0508.sample3.ExampleFragment3
 
 class FragmentAdapter(private val context: Context, manager: FragmentManager) : FragmentPagerAdapter(manager) {
     override fun getItem(position: Int): Fragment {
         return when (position) {
             0 -> ExampleFragment1()
             1 -> ExampleFragment2()
+            2 -> ExampleFragment3()
             else -> throw IllegalStateException()
         }
     }
@@ -20,9 +22,10 @@ class FragmentAdapter(private val context: Context, manager: FragmentManager) : 
         return when (position) {
             0 -> context.getString(R.string.example_fragment_1_title)
             1 -> context.getString(R.string.example_fragment_2_title)
+            2 -> context.getString(R.string.example_fragment_3_title)
             else -> throw IllegalStateException()
         }
     }
 
-    override fun getCount() = 2
+    override fun getCount() = 3
 }
